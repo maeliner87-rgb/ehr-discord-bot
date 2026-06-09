1ccf2decb3b361b4e5bf603cdc621008c26791f2
maeliner87-rgb <maeliner87@gmail.com>
2026-06-09 08:18:25 +0200

    Update permis.py

--- permis.py
+++ permis.py
@@ -1,1 +1,23 @@
+import discord
+from discord import app_commands
+from discord.ui import View, Button
+import psycopg2
+import os
 
+conn = psycopg2.connect(os.getenv("DATABASE_URL"))
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS permis (
+    pseudo_roblox TEXT PRIMARY KEY,
+    nom TEXT,
+    prenom TEXT,
+    date_obtention TEXT,
+    points INTEGER,
+    categorie TEXT,
+    statut TEXT,
+    salon_demande BIGINT,
+    valide INTEGER DEFAULT 0
+)
+""")
+conn.commit()
